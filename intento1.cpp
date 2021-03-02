5d2814c69aabcf1c686d666c8b5fe055fcd3daad
Alejandro <alegomezse@unal.edu.co>
2021-03-02 15:06:41 -0500

    ahora multiplica

--- intento1.cpp
+++ intento1.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <cstdlib>
 int main(int argc,char **argv){
+    int a = std::atoi(argv[1]);
+    int b = a*2;
     std::cout<<"porfin lo lograste de verdad verdad \n"<<std::endl;
-    return 0;
+    std::cout<<"numero: "<< b << std::endl;
+    return b;
 
 }
