a70920113db0fdee6a0fa5b4c03808e94c09893d
Alejandro <alegomezse@unal.edu.co>
2021-03-02 14:25:05 -0500

    de verdad se hizo

--- intento1.cpp
+++ intento1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main(int argc,char **argv){
-    std::cout<<"porfin lo lograste \n"<<std::endl;
+    std::cout<<"porfin lo lograste de verdad verdad \n"<<std::endl;
     return 0;
 
 }
